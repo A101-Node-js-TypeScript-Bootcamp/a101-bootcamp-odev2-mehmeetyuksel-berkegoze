5188d91fe20e6c5b30378bac47c9dcc18ffc4808
Mehmet Yuksel <mh.mehmetyuksel@gmail.com>
2022-01-20 13:43:34 +0300

    joi & jwt added

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 
-
 const route = require("./router/api")
+const jwt = require("./helpers/jwt");
 
+app.use(jwt());
 app.use(express.json());
 
-app.use("/api", route)
 
+app.use("/api", route)
 
 
 const port = 3000;
--- router/user/user.js
+++ router/user/user.js
@@ -1,8 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
-const userController = require("../../controller/userController")
-router.get("/login" , userController.loginUser)
 
+const userController = require("../../controller/userController")
+router.post("/" , userController.loginUser)
 
 module.exports = router;
