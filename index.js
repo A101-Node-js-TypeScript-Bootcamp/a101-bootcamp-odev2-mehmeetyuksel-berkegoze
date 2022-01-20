93b2e4e24d8cc7080c4b317f6871ee17fbbfbab7
gozeberke <73718014+gozeberke@users.noreply.github.com>
2022-01-20 13:50:25 +0300

    errorHandler added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 const route = require("./router/api")
 const jwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/errorHandler")
 
 app.use(jwt());
 app.use(express.json());
@@ -10,7 +11,7 @@ app.use(express.json());
 
 app.use("/api", route)
 
-
+app.use(errorHandler);
 const port = 3000;
 app.listen(port, () => {
     console.log("Server ayağa kaldırıldı")
